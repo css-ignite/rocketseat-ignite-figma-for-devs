4b9c3aa2a558fe8cd4005877604d92ac794710fb
claudneysessa <claudneysessa@users.noreply.github.com>
2022-10-17 05:43:14 +0000

    style: Update design tokens

--- build/js/tokens-js.js
+++ build/js/tokens-js.js
@@ -1,14 +1,18 @@
 /**
  * Do not edit directly
- * Generated on Mon, 17 Oct 2022 04:35:10 GMT
+ * Generated on Mon, 17 Oct 2022 05:43:14 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorGray200": "#898996",
   "ColorGray400": "#50505c",
   "ColorGray700": "#1c1c1f",
   "ColorGray900": "#121214",
   "ColorPurple500": "#8b5cf6",
+  "ColorPurpleGray100": "#453667",
   "ColorWhite900": "#ffffff",
   "ColorBlack900": "#000000"
 };
