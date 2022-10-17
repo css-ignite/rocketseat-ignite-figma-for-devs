0435533e26463028cc235042bfe36a81a01abc40
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2022-10-17 01:50:05 -0300

    Configurando mais 2 scripts de geração para JavaScript e Dart/Flutter

---
+++ build/flutter/tokens-flutter.dart
@@ -0,0 +1,23 @@
+
+//
+// tokens-flutter.dart
+//
+
+// Do not edit directly
+// Generated on Mon, 17 Oct 2022 04:35:10 GMT
+
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorBlack900 = Color(0xFF000000);
+    static const colorGray200 = Color(0xFF898996);
+    static const colorGray400 = Color(0xFF50505C);
+    static const colorGray700 = Color(0xFF1C1C1F);
+    static const colorGray900 = Color(0xFF121214);
+    static const colorPurple500 = Color(0xFF8B5CF6);
+    static const colorWhite900 = Color(0xFFFFFFFF);
+}
---
+++ build/js/tokens-js.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 17 Oct 2022 04:35:10 GMT
+ */
+
+module.exports = {
+  "ColorGray200": "#898996",
+  "ColorGray400": "#50505c",
+  "ColorGray700": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6",
+  "ColorWhite900": "#ffffff",
+  "ColorBlack900": "#000000"
+};
